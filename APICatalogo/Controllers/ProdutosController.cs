1eff2452635c40378995a8ef76f2da148698d80a
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-10-11 11:09:54 -0300

    Criação métodos para Alteração e Exclusão de produtos

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -40,13 +40,57 @@ namespace APICatalogo.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] Produto produto)
         {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            //NÃO É NECESSÁRIO VERIRICAR SE MODELSTATE É VÁLIDO POR CONTA DO ATTRIBUTE [Route("api/[controller]")]
+            //if (!ModelState.IsValid)
+            //    return BadRequest(ModelState);
 
             _context.Produtos.Add(produto);
             _context.SaveChanges();
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Produto produto)
+        {
+            //if (!ModelState.IsValid)
+            //    return BadRequest(ModelState);
+
+            if (id != produto.Id)
+                return BadRequest();
+            //Minha versão
+            //var _produto = _context.Produtos.Find(id);
+
+            //_produto.Descricao = produto.Descricao;
+            //_produto.ImagemUrl = produto.ImagemUrl;
+            //_produto.Nome = produto.Nome;
+            //_produto.Preco = produto.Preco;
+            //_produto.Estoque = produto.Estoque;
+            //_produto.CategoriaId = produto.CategoriaId;
+
+            //_context.Produtos.Update(_produto);
+            //_context.SaveChanges();
+
+            //Versão do professor
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            //var produto = _context.Produtos.FirstOrDefault(x => x.Id == id);
+            var produto = _context.Produtos.Find(id); //Obs: Vantagem do método Find, ele busca primeiro em memória, se não achar ele vai até o banco de dados, FirstOrDefault vai direto ao banco.
+
+            if (produto == null)
+                return NotFound();
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return produto;
         }
     }
 }
