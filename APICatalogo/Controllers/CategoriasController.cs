1d798e7adfa16496f036d84115b9c67ca0058f3f
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-10-13 10:50:30 -0300

    Controller de categorias

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,75 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            return _context.Categorias.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Categoria categoria)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (id != categoria.Id)
+                return BadRequest();
+
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.Find(id); 
+
+            if (categoria == null)
+                return NotFound();
+
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+
+            return categoria;
+        }
+
+
+    }
+}
