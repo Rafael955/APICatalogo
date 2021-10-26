daee7d29ed23f326b80eae51c76a9dabcbaf24c3
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-10-26 09:25:59 -0300

    ajuste da propriedade de preco da classe produto

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -20,6 +20,8 @@ namespace APICatalogo.Models
         public string Descricao { get; set; }
 
         [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(8,2)")]
         [Range(1, 10000, ErrorMessage = "O preço deverá estar entre {1} e {2}")]
         public decimal Preco { get; set; }
 
