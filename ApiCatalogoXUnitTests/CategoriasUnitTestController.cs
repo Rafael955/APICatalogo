6430d9447ceb8ba644b079d8e085e4c1c04c64bc
Rafael Caffonso <rafaelcaffonso@gmail.com>
2021-11-04 11:21:37 -0300

    Testes unitários

---
+++ ApiCatalogoXUnitTests/CategoriasUnitTestController.cs
@@ -0,0 +1,52 @@
+﻿using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repository;
+using APICatalogo.Repository.Interfaces;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoXUnitTests
+{
+    public class CategoriasUnitTestController
+    {
+        private IMapper mapper;
+        private IUnitOfWork repository;
+
+        public static DbContextOptions<ApplicationDbContext> dbContextOptions { get; }
+
+        public static string connectionString = "Server=localhost;Database=CatalogoDB;Uid=root;Pwd=1234";
+
+
+        //Construtores estáticos são inicializados automaticamente antes dos outros.
+        static CategoriasUnitTestController() 
+        {
+            dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                .Options;
+        }
+
+        public CategoriasUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+
+            mapper = config.CreateMapper();
+
+            var context = new ApplicationDbContext(dbContextOptions);
+
+            //DBUnitTestsMockInitializer db = new DBUnitTestsMockInitializer();
+            //db.Seed(context);
+
+            repository = new UnitOfWork(context);
+        }
+
+        //testes unitários
+    }
+}
---
+++ ApiCatalogoXUnitTests/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,26 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+
+namespace ApiCatalogoXUnitTests
+{
+    public class DBUnitTestsMockInitializer
+    {
+        public DBUnitTestsMockInitializer()
+        {
+
+        }
+
+        public void Seed(ApplicationDbContext context)
+        {
+            context.Categorias.Add(new Categoria { Id = 999, Nome = "Bebidas999", ImagemUrl = "bebidas999.jpg" });
+
+            context.Categorias.Add(new Categoria { Id = 8, Nome = "Sucos", ImagemUrl = "sucos.jpg" });
+
+            context.Categorias.Add(new Categoria { Id = 9, Nome = "Doces", ImagemUrl = "doces.jpg" });
+
+            context.Categorias.Add(new Categoria { Id = 10, Nome = "Salgados", ImagemUrl = "salgados.jpg" });
+
+            context.Categorias.Add(new Categoria { Id = 11, Nome = "Tortas", ImagemUrl = "tortas.jpg" });
+        }
+    }
+}
